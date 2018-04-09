f8d0f791d5cdf3f62ed3f29b6f60a2c14944b7eb
Dan Watson <danwatson00@yahoo.com>
2018-04-09 14:30:55 -0500

    got cards working and bootstrap installed

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Header from './Header';
 import Footer from './Footer';
 import bannerImg from '../images/pano01.png';
 import Banner from './Banner';
+import Cards from './Cards';
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
       <div className="App">
         <Header title="Wide World Travel Inc"/>
         <img className="App-banner" src={bannerImg} />
+        <Cards />
         <Footer />
       </div>
     );
